2755f8e56f7f772c0803e469816ea67e0c9c1017
kinkimy <148370963+kinkimy@users.noreply.github.com>
2025-04-30 21:19:51 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const checkBox = document.getElementById('checkbox');
+function Menu(){
+    var menu = document.getElementById("menuinicial");
+    if(menu.style.display ==="none"){
+        menu.style.display="block";
+    }
+    else{
+        menu.style.display="none";
+    }
+}
+
+/*const checkBox = document.getElementById('checkbox');
 const container = document.getElementById('container');
 
 checkBox.addEventListener("change", ()=>{
     container.classList.toggle('dark');
-})
+})*/
