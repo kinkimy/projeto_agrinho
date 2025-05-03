af40b159801af21beeafc829e9831f9badd7c841
kinkimy <148370963+kinkimy@users.noreply.github.com>
2025-05-03 12:31:43 -0300

    .

--- script.js
+++ script.js
@@ -7,10 +7,3 @@ function Menu(){
         menu.style.display="none";
     }
 }
-
-/*const checkBox = document.getElementById('checkbox');
-const container = document.getElementById('container');
-
-checkBox.addEventListener("change", ()=>{
-    container.classList.toggle('dark');
-})*/
