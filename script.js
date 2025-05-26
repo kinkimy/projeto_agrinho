2e38dc06a82b3020cb3de544a264e12385524c83
kinkimy <148370963+kinkimy@users.noreply.github.com>
2025-05-25 23:48:45 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleBotaoMenu() {
+function botaoMenu() {
     const menu= document.getElementById('accessibility-menu');
 
     if(menu.style.display === "block"){
@@ -6,4 +6,11 @@ function toggleBotaoMenu() {
     }else{
         menu.style.display = "block";
     }
+}
+
+function increaseFont() {
+    var element = document.querySelector("body");
+    var tamanhoAtual = window.getComputedStyle(element).fontSize;
+    var newSize = parseFloat(newSize) + 1 + "px";
+    element.style.fontSize = newSize
 }
