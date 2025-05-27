09015c42153cd0b033afa9be11f983a73349a07e
kinkimy <148370963+kinkimy@users.noreply.github.com>
2025-05-26 21:40:04 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function botaoMenu() {
-    const menu= document.getElementById('accessibility-menu');
 
-    if(menu.style.display === "block"){
-        menu.style.display = "none";
-    }else{
-        menu.style.display = "block";
+
+('text-small', 'text-medium', 'text-large', 'text-xlarge', 'text-xxlarge'
+);
+
+aumentarFonte.addEventListener('click', () => {
+    if(currentFontSizeIndex < fontSizeLevels.length - 1){
+        currentFontSizeIndex ++;
+        uptadeFontSize();
     }
+})
+
+diminuirFonte.addEventListener('click', () => {
+    if(currentFontSizeIndex > 0){
+        currentFontSizeIndex--;
+        uptadeFontSize();
+    }
+});
+
+const savedFontSize = localStorage.getItem('fontSize');
+if(savedFontSize) {
+    currentFontSizeIndex = parseInt(savedFontSize);
+    updateFontSize();
 }
+
+// function botaoMenu() {
+//     const menu= document.getElementById('accessibility-menu');
+
+//     if(menu.style.display === "block"){
+//         menu.style.display = "none";
+//     }else{
+//         menu.style.display = "block";
+//     }
+// }
