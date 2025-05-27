7beac172db659a955f360af42d2c3f7f2e193b77
kinkimy <148370963+kinkimy@users.noreply.github.com>
2025-05-26 23:11:44 -0300

    .

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-[
-    {class: 'text-small', label: 'Pequeno'},
-    {class: 'text-medium', label: 'Médio'},
-    {class: 'text-large', label: 'Grande'},
-    {class: 'text-xlarge', label: 'Extra Grande'},
-    {class: 'text-xxlarge', label: 'Muito Grande'},
-];
-
-let currentFontSizeIndex = 1;
-
-const aumentarFonte = document.getElementById('aumentarFonte');
-const diminuirFonte = document.getElementById('diminuirFonte');
-const currentFontSize = document.getElementById('currentFontSize');
-
-function updateFontSize(){
-    document.body.classList.remove(
-        'text-small', 'text-medium', 'text-large', 'text-xlarge', 'text-xxlarge'
-    );
-
-    document.body.classList.add(fontSizeLevels[currentFontSizeIndex].class);
-    currentFontSize.textContent = fontSizeLevels[currentFontSizeIndex].label;
-    localStorage.setItem('fontSize', currentFontSizeIndex);
-}
-
-aumentarFonte.addEventListener('click', () => {
-    if(currentFontSizeIndex < fontSizeLevels.length - 1){
-        currentFontSizeIndex ++;
-        uptadeFontSize();
-    }
-})
-
-diminuirFonte.addEventListener('click', () => {
-    if(currentFontSizeIndex > 0){
-        currentFontSizeIndex--;
-        uptadeFontSize();
-    }
-});
-
-const savedFontSize = localStorage.getItem('fontSize');
-if(savedFontSize) {
-    currentFontSizeIndex = parseInt(savedFontSize);
-    updateFontSize();
-}
-
  function botaoMenu() {
      const menu= document.getElementById('accessibility-menu');
 
@@ -51,3 +7,12 @@ if(savedFontSize) {
          menu.style.display = "block";
      }
  }
+
+ const botaoDiminuir = document.getElementById("diminuir-fonte");
+
+ botaoDiminuir .addEventListener("click", () => {
+    const textElement = document.body;
+    let tamanhoAtual = parseFloat(window.getComputedStyle(textElement).fontSize);
+    tamanhoAtual -= 1;
+    textElement.style.fontSize = tamanhoAtual + "px";
+ })
